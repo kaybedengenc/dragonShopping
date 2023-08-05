05af6c5e4fe3304d379a65c65469abe84d02074a
kaybedengenc <86600469+kaybedengenc@users.noreply.github.com>
2023-08-05 12:00:40 +0300

    github pages adres güncellemesi
    github pages adres güncellemesi hosting de json sayfası için güncelleme yapıldı

--- cardcollector.js
+++ cardcollector.js
@@ -13,7 +13,7 @@ async function loadcards()
    }
    
     try {
-        const response = await fetch("http://127.0.0.1:5500/card.json");
+        const response = await fetch("https://kaybedengenc.github.io/dragonShopping/card.json");
         let cardsJson = await response.json();
         //console.log(cardsJson);
         var cards = cardsJson.map(function(card) {
